2aa6f9d83d88a1a067f3dcf53a5f10410e6b8cb7
FedericoPorcel64 <126525656+FedericoPorcel64@users.noreply.github.com>
2025-05-10 10:00:48 -0300

    Probando Cambiando el Register

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "V7";
+const CACHE = "V9";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const urlsToCache= [
