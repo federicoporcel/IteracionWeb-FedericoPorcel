895342cd126695774d423853cd14124abbb77ba0
FedericoPorcel64 <126525656+FedericoPorcel64@users.noreply.github.com>
2025-05-10 10:07:24 -0300

    Probando agregando "enctype": "multipart/form-data"

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "V9";
+const CACHE = "V10";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const urlsToCache= [
