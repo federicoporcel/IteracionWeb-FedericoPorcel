5495ae3ac6516466ecb33a47cb224f93f851ac37
FedericoPorcel64 <126525656+FedericoPorcel64@users.noreply.github.com>
2025-05-11 21:10:31 -0300

    Actualizando Cache

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "V11";
+const CACHE = "V12";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const urlsToCache= [
