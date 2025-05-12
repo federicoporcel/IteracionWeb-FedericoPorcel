98e280c4a694c6f4c3b2ca2ccd5217f9beb05525
FedericoPorcel64 <126525656+FedericoPorcel64@users.noreply.github.com>
2025-05-11 22:21:46 -0300

    V18 - Modificando fetch

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "V17";
+const CACHE = "V18";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const urlsToCache= [
@@ -45,12 +45,12 @@ self.addEventListener('fetch', (event) => {
         }
 
         const networkResp = await fetch(event.request);
-        console.log("Hizo el Try");
+        //console.log("Hizo el Try");
         return networkResp;
       } catch (error) {
 
         const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(urlsToCache);        
+        const cachedResp = await cache.match(event.request);        
         return cachedResp;
       }
     })());
