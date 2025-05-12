d11ece3eaca973f8661e0512af1a8dfd737f08d0
FedericoPorcel64 <126525656+FedericoPorcel64@users.noreply.github.com>
2025-05-11 21:37:01 -0300

    Modificar nombre de cache

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "V14";
+const CACHE = "V15";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const urlsToCache= [
