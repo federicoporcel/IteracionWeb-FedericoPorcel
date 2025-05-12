fc35a6d563add5803b98b00a76a3549de81b36c8
FedericoPorcel64 <126525656+FedericoPorcel64@users.noreply.github.com>
2025-05-11 21:46:57 -0300

    Agregando Logs

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "V15";
+const CACHE = "V16";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const urlsToCache= [
@@ -25,10 +25,13 @@ self.addEventListener('install', async (event) => {
     caches.open(CACHE)
       .then((cache) => cache.add(urlToCache))
   );
+  console.log("Agregó la primer URL");
   event.waitUntil(
     caches.open(CACHE)
       .then((cache) => cache.add(urlToCache2))
+      
   );
+  console.log("Agregó la segunda URL");
 });
 
 if (workbox.navigationPreload.isSupported()) {
@@ -39,6 +42,7 @@ self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
     event.respondWith((async () => {
       try {
+        
         const preloadResp = await event.preloadResponse;
 
         if (preloadResp) {
@@ -46,11 +50,12 @@ self.addEventListener('fetch', (event) => {
         }
 
         const networkResp = await fetch(event.request);
+        console.log("Hizo el Try");
         return networkResp;
       } catch (error) {
 
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(urlToCache);
+        var cache = await caches.open(CACHE);
+        var cachedResp = await cache.match(urlToCache);
         
         cache = await caches.open(CACHE);
          cachedResp = await cache.match(urlToCache2);
